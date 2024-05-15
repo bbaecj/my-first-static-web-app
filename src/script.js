b1401dc06cddd2b59434d55c5de0cee30a55d62b
bbaecj <bbaecj@bbcag.ch>
2024-05-15 10:46:51 +0200

    Delete src/script.js

--- src/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuButton = document.querySelector('.menu-button');
-    const nav = document.querySelector('nav');
-
-    menuButton.addEventListener('click', function() {
-        nav.classList.toggle('active');
-    });
-});
